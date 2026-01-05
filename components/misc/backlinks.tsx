0e5aad951e4602f60f3c1ddf58d7edf1d93e3c01
eramoss <eramossl@outlook.com>
2026-01-05 10:53:38 -0300

    just fixes new version

--- components/misc/backlinks.tsx
+++ components/misc/backlinks.tsx
@@ -16,7 +16,7 @@ const Backlinks = ({ backlinks }: Props) => {
       {Object.keys(backlinks).map((slug) => {
         const post = backlinks[slug]
         return (
-          <Link as={slug} href="[...slug]" className="col-span-1">
+          <Link as={slug} href="[...slug]" key={slug} className="col-span-1">
             <NotePreview title={post.title} content={post.excerpt} />
           </Link>
         )
